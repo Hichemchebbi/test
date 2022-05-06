9acd5e0e1238f4821e104e53a44d310d80ba9412
hichem chebbi <hichem.chebbi@isticbc.org>
2022-05-06 12:03:42 +0100

    modif terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,9 +3,23 @@ resource "aws_instance" "ubuntu-instance" {
   instance_type = var.instance_type
   key_name      = "mykey"
   security_groups = ["${aws_security_group.UbuntuSG.name}"]
+
+#terraform remote exec 
+  provisioner "file" {
+    source      = "userdata.sh"
+    destination = "/tmp/userdata.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/userdata.sh",
+      "/tmp/userdata.sh args",
+    ]
+  }
+
   tags  = {
     Name  = "Jenkins-server"
   }
   user_data = file("userdata.sh")
 }
-#terraform remote exec 
+
--- terraform/security.tf
+++ terraform/security.tf
@@ -13,7 +13,12 @@ resource "aws_security_group" "UbuntuSG" {
     protocol  = "tcp"
     cidr_blocks  = ["0.0.0.0/0"]
   }
-
+  ingress {
+    from_port = 8080
+    to_port   = 8080
+    protocol  = "tcp"
+    cidr_blocks  = ["0.0.0.0/0"]
+  }
     ingress {
     from_port = 443
     to_port   = 443
