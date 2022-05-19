bd7bae00a738920e5447154d5e9f450bd9414118
hichem chebbi <91631899+Hichemchebbi@users.noreply.github.com>
2022-05-19 12:28:35 +0100

    Update userdata.sh

--- terraform/userdata.sh
+++ terraform/userdata.sh
@@ -5,6 +5,7 @@ sudo apt-get update -y
 sudo apt upgrade -y 
 sudo apt-get install openjdk-8-jdk -y
 #Install Jenkins 
+
 echo "--------------------Installing Jenkins--------------------"
 sudo apt -y install wget
 sudo wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
